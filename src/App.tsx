2da3ca1eaed772d783e3e9c1cf1a39f8fff83d76
devxyn <johnlawrencehermo@gmail.com>
2025-10-23 23:21:15 +0800

    feat: Integrate Profile component into App layout for enhanced user experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <Navbar />
+      <main className='md:ml-[10vw] lg:ml-[16vw] flex flex-col items-center'>
+        <section className='w-full md:w-[75%] flex flex-col items-center mt-10 md:mt-16'>
+          <Profile />
+        </section>
+      </main>
     </>
   );
 }
